0963e18345805336b0abe112d706d06c44309c5b
Yan Lukavii <mr.winston93@mail.ru>
2022-10-31 15:33:32 +0300

    hw03 исправления

--- hw03-annotations/src/main/java/ru/otus/annotations/usage/TestRunner.java
+++ hw03-annotations/src/main/java/ru/otus/annotations/usage/TestRunner.java
@@ -4,6 +4,7 @@ import ru.otus.annotations.After;
 import ru.otus.annotations.Before;
 import ru.otus.annotations.Test;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -19,35 +20,35 @@ public class TestRunner {
         }
     }
 
-    private static void run(Class<?> clazz) throws Exception {
+    private static ArrayList<Method> getArrayListOfMethodsWithDesiredAnnotation(Method[] methodsAll, Class<?> annotationClass, Class<?> clazz) throws NoSuchMethodException {
 
-        Method[] methodsAll = clazz.getDeclaredMethods();
-
-        ArrayList<Method> listMethodsAnnotationTest = new ArrayList<>();
-        ArrayList<Method> listMethodsAnnotationBefore = new ArrayList<>();
-        ArrayList<Method> listAnnotationAfter = new ArrayList<>();
+        ArrayList<Method> list = new ArrayList<>();
 
         for (Method method : methodsAll) {
 
-            var hasAnnotationTest = clazz.getMethod(method.getName()).isAnnotationPresent(Test.class);
-            var hasAnnotationBefore = clazz.getMethod(method.getName()).isAnnotationPresent(Before.class);
-            var hasAnnotationAfter = clazz.getMethod(method.getName()).isAnnotationPresent(After.class);
+            var hasAnnotation = clazz.getMethod(method.getName()).isAnnotationPresent((Class<? extends Annotation>) annotationClass);
 
-            if (hasAnnotationTest) {
-                listMethodsAnnotationTest.add(method);
-            }
-            if (hasAnnotationBefore) {
-                listMethodsAnnotationBefore.add(method);
-            }
-            if (hasAnnotationAfter) {
-                listAnnotationAfter.add(method);
+            if (hasAnnotation) {
+                list.add(method);
             }
         }
+        return list;
+    }
+
+    private void run(Class<?> clazz) throws Exception {
+
+        Method[] methodsAll = clazz.getDeclaredMethods();
+
+        ArrayList<Method> listMethodsAnnotationTest = getArrayListOfMethodsWithDesiredAnnotation(methodsAll, Test.class, clazz);
+        ArrayList<Method> listAnnotationAfter = getArrayListOfMethodsWithDesiredAnnotation(methodsAll, After.class, clazz);
+        ArrayList<Method> listMethodsAnnotationBefore = getArrayListOfMethodsWithDesiredAnnotation(methodsAll, Before.class, clazz);
+
+
         int passedTests = listMethodsAnnotationTest.size();
 
         if (listMethodsAnnotationTest.size() == 0) {
 
-            System.err.println("No tests found for given includes");
+            System.err.println("No tests found for given includes... " + clazz.getName());
 
         } else {
 
@@ -76,10 +77,9 @@ public class TestRunner {
 
     }
 
-
     public static void main(String[] args) throws Exception {
 
-        run(TestClass.class);
+        new TestRunner().run(TestClass.class);
 
     }
 
