6ec359c544b3a1c6ebac75891a9ce57284edce11
Yan Lukavii <mr.winston93@mail.ru>
2022-12-15 17:56:13 +0300

    hw 08 правки ResourcesFileLoader на Jackson

--- hw08-io/src/main/java/ru/otus/dataprocessor/FileProcessException.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/FileProcessException.java
@@ -1,5 +1,7 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public class FileProcessException extends RuntimeException {
     public FileProcessException(Exception ex) {
         super(ex);
@@ -10,6 +12,9 @@ public class FileProcessException extends RuntimeException {
     }
 
     public FileProcessException(String msg, NullPointerException e) {
+        super(msg,e);
+    }
+    public FileProcessException(String msg, JsonProcessingException e) {
         super(msg,e);
     }
 }
--- hw08-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,8 +1,11 @@
 package ru.otus.dataprocessor;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.json.Json;
 import ru.otus.model.Measurement;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,13 +24,17 @@ public class ResourcesFileLoader implements Loader {
         try (var jsonReader = Json.createReader(ResourcesFileLoader.class.getClassLoader()
                 .getResourceAsStream(filename))) {
             var jsonFromTheFile = jsonReader.read();
+            ObjectMapper objectMapper = new ObjectMapper();
             Object[] obj = jsonFromTheFile.asJsonArray().toArray();
-            var gson = new Gson();
+            JsonNode jsonNode;
             for (Object object : obj) {
-                loadedMeasurementList.add(gson.fromJson(object.toString(), Measurement.class));
+                jsonNode = objectMapper.readTree(object.toString());
+                loadedMeasurementList.add(new Measurement(jsonNode.get("name").asText(),jsonNode.get("value").doubleValue()));
             }
         } catch (NullPointerException e) {
             throw new FileProcessException("Проблема с файлом", e);
+        } catch (JsonProcessingException e) {
+            throw new FileProcessException("Проблема в содержимом файла", e);
         }
         return loadedMeasurementList;
     }
